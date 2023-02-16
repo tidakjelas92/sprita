b506172c27b733fc162f3c6ba564cd0e6ce86ee6
hansen <hansenan14@gmail.com>
2023-02-16 18:06:27 +0700

    feat: short description of command line options

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,19 +1,25 @@
 use clap::Parser;
 
 #[derive(Parser)]
+#[command(author, version, about, long_about = None)]
 pub struct Arguments {
+    /// Path to a singular file or an entire directory.
     #[arg(short, long)]
     pub input: String,
 
+    /// Must be path to a singular file if the input is a singular file, and vice versa.
     #[arg(short, long)]
     pub output: String,
 
+    /// Optional flag to overwrite files at the output path.
     #[arg(short, long)]
     pub force: bool,
 
+    /// Optional flag to downsize the image according to a max_size.
     #[arg(short, long)]
     pub downsize: bool,
 
+    /// Must be specified if downsize is specified.
     #[arg(short, long)]
     pub max_size: Option<u32>
 }
